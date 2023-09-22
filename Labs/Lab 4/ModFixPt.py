d6b4a54a556885e998dc209fa412e1728a93a667
DiegChav <dich3116@colorado.edu>
2023-09-21 19:41:20 -0600

    Updated Lab 4

--- Labs/Lab 4/ModFixPt.py
+++ Labs/Lab 4/ModFixPt.py
@@ -12,14 +12,42 @@ def driver():
     xstar_approximations, ier = fixedpt(f1, x0, tol, Nmax)
     print('Approximate fixed point iterations for f1:', xstar_approximations)
     print('f1(xstar):', f1(xstar_approximations[-1]))
+    print("Fixed point converges in:", len(xstar_approximations))
     print('Error message reads:', ier)
 
-    # test f2
-    x0 = 0.0
-    xstar_approximations, ier = fixedpt(f2, x0, tol, Nmax)
-    print('Approximate fixed point iterations for f2:', xstar_approximations)
-    print('f2(xstar):', f2(xstar_approximations[-1]))
-    print('Error message reads:', ier)
+    accelerated_sequence = aitkens_method(xstar_approximations, tol, Nmax)
+    print("Accelerated sequence:", accelerated_sequence)
+    print("Accelerated sequence converges in:", len(accelerated_sequence))
+
+def aitkens_method(approximations, tol, Nmax):
+   
+    n = len(approximations)
+    
+    accelerated_sequence = np.zeros(n-2)  # Store the accelerated sequence
+    
+    for i in range(n-2):
+        a_i = approximations[i]
+        a_ip1 = approximations[i+1]
+        a_ip2 = approximations[i+2]
+        
+        # Aitken's formula to calculate the accelerated sequence
+        accelerated_sequence[i] = a_i - ((a_ip1 - a_i)**2) / (a_ip2 - 2*a_ip1 + a_i)
+    
+    # Check for convergence using tolerance
+    for i in range(Nmax):
+        if np.abs(accelerated_sequence[-1] - accelerated_sequence[-2]) < tol:
+            break
+        
+        a_i = accelerated_sequence[-2]
+        a_ip1 = accelerated_sequence[-1]
+        a_ip2 = accelerated_sequence[-2]
+        
+        # Update the accelerated sequence
+        accelerated_sequence = np.append(accelerated_sequence, a_i - ((a_ip1 - a_i)**2) / (a_ip2 - 2*a_ip1 + a_i))
+    
+    return accelerated_sequence
+
+
 
 # Modified the fixedpt function to return a list of approximations
 def fixedpt(f, x0, tol, Nmax):
@@ -45,5 +73,6 @@ def fixedpt(f, x0, tol, Nmax):
 
     ier = 1
     return xstar_approximations, ier
+
 
 driver()
