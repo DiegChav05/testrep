f6bbec8a1846e25ea8adc35e24d446570e1e7dd7
DiegChav <dich3116@colorado.edu>
2023-09-28 15:05:54 -0600

    Finished Lab 5

--- Labs/Lab 5/Lab5.py
+++ Labs/Lab 5/Lab5.py
@@ -28,38 +28,26 @@ def newton(f, f_prime, x0, tol, Nmax):
 
 
 
-def newtons_method(x0, tol, max_iterations, f, f_prime):
-    # Newton's method
-    x = x0
-    for i in range(max_iterations):
-        x_new = x - f(x) / f_prime(x)
-        if abs(x_new - x) < tol:
-            return x_new, i+1  # Convergence achieved, return the root and the number of iterations
-        x = x_new
-    return None, max_iterations  # Return None if max_iterations reached without convergence
 
 def bisection_to_newton(tol, a, b, f, f_prime):
     # Bisection method
-    num_bisection_iterations = 0
+    num_iterations = 0
     while (b - a) / 2 >= tol:
         midpoint = (a + b) / 2
 
         if f(midpoint) == 0 or f_prime(midpoint) == 0:
-            return midpoint, num_bisection_iterations  # Return if midpoint is a root or division by zero
+            return midpoint, num_iterations  # Return if midpoint is a root or division by zero
 
         if f(midpoint) * f(a) < 0:
             b = midpoint
         else:
             a = midpoint
 
-        num_bisection_iterations += 1
+        num_iterations += 1
 
-    # Check if midpoint is in the basin of convergence for Newton's method
-    if abs(f_prime(midpoint)) < tol:
-        return midpoint, num_bisection_iterations
-    else:
-        return None, num_bisection_iterations  # Return None if midpoint is not in the basin of convergence
-
+    root = midpoint - f(midpoint) / f_prime(midpoint)
+    return root,num_iterations
+   
 tol = 1e-10
 
 # Define the function and its derivative using lambda functions
@@ -84,12 +72,12 @@ print("Number of iterations for Newton's method:", np.ceil(iterations))
 
 a = 2
 b = 4.5
-tol = 1e-10
+
 
 # Perform bisection and check for basin of convergence for Newton's method
-midpoint, num_bisection_iterations = bisection_to_newton(tol, a, b, f, f_prime)
-print("Root found using Bisection method:", midpoint)
-print("Number of iterations for Bisection method:", num_bisection_iterations)
+root, num_iterations = bisection_to_newton(tol, a, b, f, f_prime)
+print("Root found using Hybrid method:", root)
+print("Number of iterations for Hybrid method:", num_iterations)
 
 
 
