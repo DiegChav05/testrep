44347f263269ceb17ff21d94d5c2f7801f8fd233
DiegChav <dich3116@colorado.edu>
2023-11-02 15:00:36 -0600

    Lab 10

--- Labs/Lab 10/Lab10.py
+++ Labs/Lab 10/Lab10.py
@@ -4,21 +4,12 @@ import math
 import numpy.linalg as la
 from scipy.integrate import quad
 
-def eval_legendre(n, x):
-    if n == 0:
-        return [1]
-    elif n == 1:
-        return [1, x]
-    else:
-        p = [1, x]
-        for i in range(2, n + 1):
-            phi_n = ((2 * i - 1) * x * p[i - 1] - (i - 1) * p[i - 2]) / i
-            p.append(phi_n)
-        return p
+
 
 def driver():
     # Function you want to approximate
-    f = lambda x: 1 / (1 + x**2)
+    f = lambda x: math.exp(x)
+    
     
     # Interval of interest
     a = -1
@@ -26,6 +17,7 @@ def driver():
     
     # Weight function
     w = lambda x: 1.0
+    #w = lambda x: 1 / np.sqrt(1 - x**2)
     
     # Order of approximation
     n = 2
@@ -40,6 +32,8 @@ def driver():
 
     # Create a vector with exact values
     fex = np.zeros(N + 1)
+
+    print(pval)
     
     for kk in range(N + 1):
         fex[kk] = f(xeval[kk])
@@ -54,7 +48,18 @@ def driver():
     plt.legend()
     plt.show()
 
-   
+def eval_legendre(n, x):
+    if n == 0:
+        return [1]
+    elif n == 1:
+        return [1, x]
+    else:
+        p = [1, x]
+        for i in range(2, n + 1):
+            phi_n = ((2 * i - 1) * x * p[i - 1] - (i - 1) * p[i - 2]) / i
+            p.append(phi_n)
+        return p
+
 
 def eval_legendre_expansion(f, a, b, w, n, x):
     # Evaluate all the Legendre polynomials at x that are needed
@@ -65,19 +70,19 @@ def eval_legendre_expansion(f, a, b, w, n, x):
     
     for j in range(0, n + 1):
         # Make a function handle for evaluating phi_j(x)
-        phi_j = lambda x: p[j]
+        phi_j = lambda x: eval_legendre(n,x)[j]
         
         # Make a function handle for evaluating phi_j^2(x) * w(x)
-        phi_j_sq = lambda x: p[j] ** 2 * w(x)
+        phi_j_sq = lambda x: eval_legendre(n,x)[j] ** 2 * w(x) 
         
         # Use the quad function from scipy to evaluate normalizations
-        norm_fac, _ = quad(phi_j_sq, a, b)
+        norm_fac, err = quad(phi_j_sq, a, b)
         
         # Make a function handle for phi_j(x) * f(x) * w(x) / norm_fac
         func_j = lambda x: phi_j(x) * f(x) * w(x) / norm_fac
         
         # Use the quad function from scipy to evaluate coeffs
-        aj, _ = quad(func_j, a, b)
+        aj, err = quad(func_j, a, b)
         
         # Accumulate into pval
         pval = pval + aj * p[j]
